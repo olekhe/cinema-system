29f4d98a0383279444bfc6fb956b2ecd6b82b5c0
Oleksandr Herhelezhyu <o.herhelezhyu@xtoniq.com>
2026-02-23 14:34:22 +0100

    Added manifests + getway api

--- _helpscripts/docker-deploy.ps1
+++ _helpscripts/docker-deploy.ps1
@@ -2,4 +2,4 @@
 docker build -t cinema.showtimes:latest .
 
 # run the docker container
-ocker run -p 8080:80 --env ASPNETCORE_ENVIRONMENT=Production cinema.showtimes:latest
+docker run -p 8080:80 --env ASPNETCORE_ENVIRONMENT=Production cinema.showtimes:latest
